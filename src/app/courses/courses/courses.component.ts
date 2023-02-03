13b788d8b62ebb4e0a6f229f21132b44c9889d29
Rayan Wilbert <rayan.rmw@gmail.com>
2023-02-03 16:39:51 -0300

    Handling request errors and using MatDialog

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,10 @@
-import { Observable } from 'rxjs';
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
+import { Observable, of, tap } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses',
@@ -12,10 +15,23 @@ export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list().pipe(
+      catchError((error) => {
+        this.onError('Error on loading courses.');
+        return of([]);
+      })
+    );
   }
 
-  ngOnInit(): void {
+  onError(errorMessage: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMessage
+    })
   }
+
+  ngOnInit(): void {}
 }
